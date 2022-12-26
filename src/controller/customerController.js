f782a5ad78fb050c9e62161a0ced0659287c216d
akashGg99 <aakkash15r@gmail.com>
2022-12-26 10:29:08 +0530

    added customer API handlers

---
+++ src/controller/customerController.js
@@ -0,0 +1,43 @@
+const customerModel = require("../model/customerModel")
+
+const getCustomer = async function(req,res){
+    try{
+
+        const custData = await customerModel.find({status:"Active"})
+
+        res.status(200).send({status:true,message:"Success", data:custData})
+    }
+    catch(err){
+        res.status(500).send({status:false,message:err.message})
+    }
+}
+
+
+const deletecustomer = async function(req,res){
+    try{
+        const data1= req.body.customerId
+        // if(sdc){}
+        const delCust = await customerModel.deleteOne({ _id:data1 })
+
+        res.status(200).send({status:true,message:"Successfully deleted.", data:delCust})
+    } 
+    catch(err){
+        res.status(500).send({status:false,message:err.message})
+    }
+}
+
+
+
+
+const createCust = async function(req,res){
+    try{
+        const data1 = req.body
+
+        const createddata=await customerModel.create(data1)
+    }
+    catch(err){
+        res.status(500).send({status:false,message:err.message})
+    }
+}
+
+module.exports={getCustomer,deletecustomer,createCust}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,12 @@
 const express =require('express')
 const router = express.Router()
+const {getCustomer,deletecustomer,createCust}=require('../controller/customerController')
+const {}=require('../controller/cardController')
 
 
-
-router.get("/getCustomers",)
-router.delete("/deleteCustomers",)
-router.post("/createCustomer",)
+router.get("/getCustomers",getCustomer)
+router.delete("/deleteCustomers",deletecustomer)
+router.post("/createCustomer",createCust)
 
 
 router.get("/getCards",)
